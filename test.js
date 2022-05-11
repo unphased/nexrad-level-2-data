ced3ce7b558073d9f3eba9b3d6c10265e2c1d9e3
Matt Walsh <github@netbymatt.com>
2022-05-11 14:53:07 -0500

    add group test

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const fs = require('fs/promises');
 const { Level2Radar } = require('./src/index');
 
 // const fileToLoad = './data/non-hi-res/KLOT19950413_132143.gz'; // The radar archive file to load
-const fileToLoad = './data/KLOT20220317_000842_V06'; // The radar archive file to load
+const fileToLoad = './data/consolemessage/KLOT20220509_074225_V06'; // The radar archive file to load
 // const fileToLoadCompressed = './data/KLOT20200715_230602_V06'; // The radar archive file to load
 // const fileToLoadError = './data/messagesizeerror';
 
